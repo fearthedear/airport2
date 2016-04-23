d6cde81145666a9271b9f901b230dafd30284696
fearthedear <linuskinzel@gmail.com>
2016-04-23 15:15:18 +0200

    removing moment and fix in app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require("fs");
-var moment = require("moment");
 
 var routes = require('./routes/index');
 
